aae5fb0eddc6910478d44af5ab4e80a6b8275e3f
SagYernar <sagyernar@gmail.com>
2017-10-23 18:20:19 +0600

    1

---
+++ Shop/Shop/MainWindow.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Shop
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ObservableCollection<Products> products;
+        public MainWindow()
+        {
+            InitializeComponent();
+            products = new ObservableCollection<Products>(
+                new List<Products>
+                {
+                    new Products
+                    {
+                        Id = 1,
+                        Name = "ShunHao",
+                        Price = 140,
+                        MadeIn = "China",
+                        Time = DateTime.Today.ToShortDateString()
+                        
+
+                    },
+                    new Products
+                    {
+                        Id = 2,
+                        Name = "Kola",
+                        Price = 220,
+                        MadeIn = "Usa",
+                        Time = DateTime.Today.ToShortDateString()
+                    },
+            });
+            productsGrid.ItemsSource = products;
+        }
+    }
+}
---
+++ Shop/Shop/Products.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shop
+{
+    public class Products
+    {
+        private long id;
+        private string name;
+        private string madeIn;
+        private double price;
+        private string time;
+
+        public long Id
+        {
+            get { return id; }
+            set
+            {
+                id = value;
+                OnPropertyChanged("Id");
+            }
+        }
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                name = value;
+                OnPropertyChanged("Name");
+            }
+        }
+        public string MadeIn
+        {
+            get { return madeIn; }
+            set
+            {
+                madeIn = value;
+                OnPropertyChanged("MadeIn");
+            }
+        }
+        public double Price
+        {
+            get { return price; }
+            set
+            {
+                price = value;
+                OnPropertyChanged("Price");
+            }
+        }
+        public string Time
+        {
+            get
+            {
+                return time;
+            }
+            set
+            {
+                if (DateTime.TryParse(value, out DateTime result))
+                    time = result.ToShortDateString();
+                else
+                    time = "!!!!";
+                OnPropertyChanged("Time");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string property = "")
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(property));
+            }
+        }
+
+    }
+}
